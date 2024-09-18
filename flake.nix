56f21cff03147b7f80282df71666c13463e4e292
Ivan Mincik <ivan.mincik@gmail.com>
2024-09-18 09:48:24 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "My Geospatial Flake";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://geonix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "geonix.cachix.org-1:iyhIXkDLYLXbMhL3X3qOLBtRF8HEyAbhPXjjPeYsCl0="
+    ];
+  };
+
+  inputs = {
+    geonix.url = "github:imincik/geospatial-nix";
+    nixpkgs.follows = "geonix/nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+
+        packages =
+          let
+            geopkgs = inputs.geonix.packages.${system};
+          in
+
+          {
+            qgis = (inputs.geonix.packages.${system}.qgis.override {
+              extraPythonPackages = p: [
+                pkgs.python3Packages.flask
+                geopkgs.python3-fiona
+              ];
+            });
+          };
+      };
+
+      flake = { };
+    };
+}
